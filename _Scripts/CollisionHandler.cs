4a0d9fb19e8a01daa6cdc87c307febef11d31eb9
girlinorbit <girlinorbit@gmail.com>
2023-06-02 16:04:44 -0500

    6/2/23 Backup

--- _Scripts/CollisionHandler.cs
+++ _Scripts/CollisionHandler.cs
@@ -1,10 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision other) {
-        
+    private void OnCollisionEnter(Collision other) 
+    {
+        switch (other.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("This thing is friendly.");
+                break;
+            case "Finish":
+                Debug.Log("You finished!");
+                break;
+            case "Fuel":
+                Debug.Log("You grabbed some fuel.");
+                break;
+            default:
+                ReloadLevel();
+                break;
+        }
+
+        void ReloadLevel()
+        {
+            int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(currentSceneIndex);
+        }
     }
 }
